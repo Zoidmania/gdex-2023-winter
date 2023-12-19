e46734a18d0aba8a57d589de6d33aac9e2b79a2e
Zoidmania <zoidmania.leland@gmail.com>
2023-12-18 20:20:43 -0500

    add mob spawner skeleton

--- src/main.gd
+++ src/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var mob_scene: PackedScene
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +13,7 @@ func _process(delta):
     pass
 
 # Reset the game state. Also good for initialization.
-func reset():
+func new_game():
 
     # delete all lingering mobs
     get_tree().call_group("mobs", "queue_free")
@@ -26,4 +28,49 @@ func reset():
 
     $BG_Music.play()
 
+    # Starts the "grace period" timer. Mobs don't start until the grace period ends.
+    $GameStartTimer.start()
 
+
+# Ends the game
+func end_game():
+
+    $MobTimer.stop()
+    $BG_Music.stop()
+
+
+# TO DO: accept arg for type, and maybe quantity
+func spawn_new_mob():
+
+    var mob = mob_scene.instantiate()
+
+    # choose a random location along the spawn path to place the new mob instance
+    var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+    mob_spawn_location.progress_ratio = randf()
+
+    # set mob's direction perpendicular to the path direction
+    # Nota Bene: angles are calculated in radians, not degrees, so we use ratios of Pi here. Pi
+    # radians is equal to 180 degrees.
+    var direction = mob_spawn_location.rotation + PI / 2
+    # add some randomness to the direction
+    direction += randf_range(-PI / 4, PI / 4)
+    mob.rotation = direction
+
+    # set random velocity fot hte mob
+    var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+    mob.linear_velocity = velocity.rotated(direction)
+
+    # spawn the mob by adding it to the main tree
+    add_child(mob)
+
+
+# Starts the mob timer after game start grace period ends
+func _on_game_start_timer_timeout():
+
+    $MobTimer.start()
+
+
+# Spawns a new mob every tick.
+func _on_mob_spawn_timer_timeout():
+
+    spawn_new_mob()
