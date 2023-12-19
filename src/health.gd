e6fcb6b9d1532d9aca71a78833dcd84c01027361
jgotty777 <jgottweis@gmail.com>
2023-12-19 16:50:06 -0500

    clean up health bar

--- src/health.gd
+++ src/health.gd
@@ -9,13 +9,3 @@ func _ready():
     $CurveHealth.max_value = midPoint
     $StraightHealth.min_value = midPoint 
     $StraightHealth.max_value = maxHealth
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
-
-func _on_h_slider_value_changed(health):
-    print(health)
-    $CurveHealth.value = health
-    $StraightHealth.value = health
