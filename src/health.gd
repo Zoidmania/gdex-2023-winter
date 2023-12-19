389457b784d46d60a960c32e117d845d9b59a64d
jgotty777 <jgottweis@gmail.com>
2023-12-19 16:47:58 -0500

    add  exported var for max health

--- src/health.gd
+++ src/health.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-
+@export var maxHealth: int
+    
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    var midPoint = maxHealth / 2
+    $CurveHealth.min_value = 0
+    $CurveHealth.max_value = midPoint
+    $StraightHealth.min_value = midPoint 
+    $StraightHealth.max_value = maxHealth
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
