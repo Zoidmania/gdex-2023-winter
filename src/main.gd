0cc10f9e6aa23117efc2430fd6e1f92067f8156b
Zoidmania <zoidmania.leland@gmail.com>
2023-12-18 19:43:27 -0500

    boilerplate for game init function
    Also instanced some of the skeleton scenes, like the HUD and
    background music player.

--- src/main.gd
+++ src/main.gd
@@ -9,3 +9,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+
+# Reset the game state. Also good for initialization.
+func reset():
+
+    # delete all lingering mobs
+    get_tree().call_group("mobs", "queue_free")
+
+    # place player at start position for the level
+    # TO DO: place StartPosition at starting location. Location TBD.
+    $Player.start($StartPosiiton.position)
+
+    # Show the HUD once the game starts. It's hidden when viewing the main menu.
+    # TO DO: flesh out HUD
+    $HUD.show()
+
+    $BG_Music.play()
+
+
