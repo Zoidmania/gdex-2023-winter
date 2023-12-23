2e426a32f386191d1e33065f9ea214f43c5948f1
Zoidmania <zoidmania.leland@gmail.com>
2023-12-23 14:34:07 -0500

    remove the need for the move_stats resource for movement input

--- src/components/move_input_component.gd
+++ src/components/move_input_component.gd
@@ -7,21 +7,21 @@ extends Node
 
 
 @export var move_component: MoveComponent
-@export var move_stats: MoveStats
+@export var speed: int
 
 
 ## Called on input events.
 ##
 ## Since the player ship only moves horizontally, this function builds a velocity [Vector2] using
-## [member MoveInputComponent.move_stats]'s [member MoveStats.speed] value and sets
-## [member MoveInputComponent.move_component]'s [member MoveComponent.velocity] to it.
+## [member MoveInputComponent.speed] value and sets [member MoveInputComponent.move_component]'s
+## [member MoveComponent.velocity] to it.
 func _input(event: InputEvent) -> void:
 
-    var x_axis = Input.get_axis("ui_left", "ui_right")
-    var y_axis = Input.get_axis("ui_up", "ui_down")
+    var x_axis = Input.get_axis("santa_move_left", "santa_move_right")
+    var y_axis = Input.get_axis("santa_move_up", "santa_move_down")
     var velocity = Vector2(x_axis, y_axis)
 
     if velocity.length() > 0:
-        velocity = velocity.normalized() * move_stats.speed
+        velocity = velocity.normalized() * speed
 
     move_component.velocity = velocity
