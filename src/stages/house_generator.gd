933ac9e2ec2e8b1a80c9e92c8dec8beb9eba9d6a
jgotty777 <jgottweis@gmail.com>
2024-01-04 22:12:09 -0500

    delivery counter works

--- src/stages/house_generator.gd
+++ src/stages/house_generator.gd
@@ -8,6 +8,7 @@ extends Node2D
 @export var sleigh: Sleigh
 @export var right_spawn: Marker2D
 @export var left_spawn: Marker2D
+signal delivery_completed
 # Called when the node enters the scene tree for the first time.
 func _ready():
     houseTimer.timeout.connect(spawnRightHouse) # Replace with function body.
@@ -31,7 +32,7 @@ func spawnRightHouse() -> void:
                     house.move_component.velocity = Vector2.ZERO
                 
                 for timer in get_tree().get_nodes_in_group('timer'):
-                    print(timer)
+                   
                     timer.paused = true
                     
                 for scroller in get_tree().get_nodes_in_group('background_scroll'):
@@ -46,12 +47,13 @@ func spawnRightHouse() -> void:
                    house.move_component.velocity = Vector2(0, 20)
                 
                 for timer in get_tree().get_nodes_in_group('timer'):
-                    print(timer)
+                    
                     timer.paused = false
                     
                 for scroller in get_tree().get_nodes_in_group('background_scroll'):
                     scroller.move_speed = 20
-                sleigh.move_input_component.speed = 200)     
+                sleigh.move_input_component.speed = 200
+                delivery_completed.emit())     
     elif 1 <= type and type <= 3:
         red_house.spawn(right_spawn.global_position, get_tree().current_scene, null, true)
     elif 4 <= type and type <= 6:
@@ -61,7 +63,7 @@ func spawnRightHouse() -> void:
 
 func spawnLeftHouse() -> void:
     var type = randi_range(1, 10)
-    if type == 10:
+    if type :
          var action_house: DeliveryHouse = delivery_house.spawn(
             left_spawn.global_position, get_tree().current_scene, null)    
          action_house.lockdown.connect(
@@ -71,11 +73,11 @@ func spawnLeftHouse() -> void:
                     house.move_component.velocity = Vector2.ZERO
                     
                 for timer in get_tree().get_nodes_in_group('timer'):
-                    print(timer)
+                   
                     timer.paused = true
                     
                 for scroller in get_tree().get_nodes_in_group('background_scroll'):
-                    print(scroller)
+                   
                     
                     scroller.move_speed = 0
                     
@@ -91,7 +93,11 @@ func spawnLeftHouse() -> void:
                     
                 for scroller in get_tree().get_nodes_in_group('background_scroll'):
                     scroller.move_speed = 20
-                sleigh.move_input_component.speed = 200)             
+                sleigh.move_input_component.speed = 200
+                
+                delivery_completed.emit()
+                
+                )             
     elif 1 <= type and type <= 3:
         red_house.spawn(left_spawn.global_position, get_tree().current_scene, null)
     elif 4 <= type and type <= 6:
