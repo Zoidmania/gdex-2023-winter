850a7acb4e77d094cb411de8f69f5c84b752fc75
Zoidmania <zoidmania.leland@gmail.com>
2024-01-06 17:06:14 -0500

    add pause screen layer

--- src/hud-ui/hud.gd
+++ src/hud-ui/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 class_name  game_hud
 
+
 @onready var ui_panel = $HSplitContainer/UIPanel
 @onready var delivery_labels: Control = $HSplitContainer/UIPanel/DeliveryLabels
 @onready var total_deliveries: Label = $HSplitContainer/UIPanel/DeliveryLabels/totalDeliveries
@@ -9,19 +10,28 @@ class_name  game_hud
 @onready var h_split_container: HSplitContainer = $HSplitContainer
 @onready var health_bar: PlayerHealthBar = $HSplitContainer/UIPanel/PlayerHealthBar
 
+@onready var pause_screen_layer: CanvasLayer = $PauseScreenLayer
+@onready var pause_text_layer: CanvasLayer = $PauseScreenLayer/PauseTextLayer
+
 @export var game_stats: GameStats
 # If it's a boss stage, set to true.
 @export var is_boss_stage:= false
 
 var play_area_width = ProjectSettings.get_setting("display/window/size/playable_area_width")
+var paused = false
 
 signal done_all_deliveries
+signal start_pause
+signal end_pause
 
 
 ## Init.
 ##
 ## Ensures that the split offset of the container matches the value defined in project settings.
 func _ready() -> void:
+
+    pause_screen_layer.hide()
+    pause_text_layer.hide()
 
     if is_boss_stage:
 
@@ -34,6 +44,21 @@ func _ready() -> void:
         done_deliveries.text = str(game_stats.score)
 
     h_split_container.split_offset = play_area_width
+
+
+func _process(delta: float) -> void:
+    if Input.is_action_just_pressed("ui_cancel"):
+        if paused:
+            paused = false
+            pause_screen_layer.hide()
+            pause_text_layer.hide()
+            end_pause.emit()
+        else:
+            paused = true
+            pause_screen_layer.show()
+            pause_text_layer.show()
+            start_pause.emit()
+
 
 
 func complete_delivery() -> void:
