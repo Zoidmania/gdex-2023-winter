07869d00aa265dc81089364031e945743cc41ee0
jgotty777 <jgottweis@gmail.com>
2024-01-06 21:00:10 -0500

    change scene

--- src/stages/stage_north_america.gd
+++ src/stages/stage_north_america.gd
@@ -1,14 +1,13 @@
 extends Node
 @onready var stage_north_america = $"."
 @onready var hud: game_hud = $HUD
-const KRAMPUS = preload("res://enemies/krampus.tscn")
+
 
 func _on_hud_done_all_deliveries():
     print('boss spawn') # Replace with function body.
-    var boss:Krampus = KRAMPUS.instantiate()
-    boss.position = Vector2(0, 50)
-    boss.target = $Sleigh
-    get_tree().current_scene.call_deferred("add_child", boss)
+   
+    
+    get_tree().change_scene_to_file('res://stages/stage_antarctica.tscn')
 
 
 func _on_house_generator_delivery_completed():
